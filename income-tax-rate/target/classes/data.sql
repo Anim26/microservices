3317d06072f9031a2f888696f83929c79fa05e6c
Anim26 <95927807+Anim26@users.noreply.github.com>
2023-01-25 20:47:21 +0530

    Microservices

---
+++ income-tax-rate/target/classes/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO INCOME_TAX_RATE( id, income_range, tax_rate,tax_regime) values (101,'0L-2.5L',0,'old');
+
+INSERT INTO INCOME_TAX_RATE( id, income_range, tax_rate,tax_regime) values (102,'2.5-5L',5,'old');
+
+INSERT INTO INCOME_TAX_RATE( id, income_range, tax_rate,tax_regime) values (103,'5L-10L',20,'old');
+
+INSERT INTO INCOME_TAX_RATE( id, income_range, tax_rate,tax_regime) values (104,'10L-20L',30,'old');
